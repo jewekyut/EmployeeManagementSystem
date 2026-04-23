cdd3e3e974de31671259fbf1b1f6358ee1614086
jewekyut <jewemalubay@gmail.com>
2026-04-23 19:58:13 +0800

    Updated Employee model and EmployeeService to match payroll_ci employees table

--- EmployeeManagementSystem/Models/Employee.cs
+++ EmployeeManagementSystem/Models/Employee.cs
@@ -8,13 +8,20 @@ namespace EmployeeManagementSystem.Models
 {
     public class Employee
     {
-        public string emp_id { get; set; }
-        public string emp_firstname { get; set; }
-        public string emp_lastname { get; set; }
+        public int emp_id { get; set; }
         public string emp_username { get; set; }
+        public string emp_lastname { get; set; }
+        public string emp_firstname { get; set; }
+        public string emp_middlename { get; set; }
+        public string emp_email { get; set; }
+        public string emp_contact { get; set; }
+        public string tin_num { get; set; }
+        public string sss_num { get; set; }
+        public string pagibig_num { get; set; }
+        public string philhealth_num { get; set; }
+        public string status { get; set; }
+        public int gross_salary { get; set; }
         public string position { get; set; }
         public string department { get; set; }
-        public string gross_salary { get; set; }
-        public string status { get; set; }
     }
 }
--- EmployeeManagementSystem/Services/EmployeeService.cs
+++ EmployeeManagementSystem/Services/EmployeeService.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 using EmployeeManagementSystem.Models;
@@ -9,12 +10,36 @@ namespace EmployeeManagementSystem.Services
     public class EmployeeService
     {
         private static readonly HttpClient client = new HttpClient();
-        private const string apiUrl = "http://localhost/payrollproject/index.php/employeeapi/getEmployees";
+        private const string baseUrl = "http://localhost/payrollproject/index.php/employeeapi";
 
         public async Task<List<Employee>> GetEmployeesAsync()
         {
-            var response = await client.GetStringAsync(apiUrl);
+            var response = await client.GetStringAsync($"{baseUrl}/getEmployees");
             return JsonConvert.DeserializeObject<List<Employee>>(response);
+        }
+
+        public async Task<string> AddEmployeeAsync(Employee emp)
+        {
+            var json = JsonConvert.SerializeObject(emp);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            var response = await client.PostAsync($"{baseUrl}/addEmployee", content);
+            return await response.Content.ReadAsStringAsync();
+        }
+
+        public async Task<string> UpdateEmployeeAsync(Employee emp)
+        {
+            var json = JsonConvert.SerializeObject(emp);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            var response = await client.PostAsync($"{baseUrl}/updateEmployee", content);
+            return await response.Content.ReadAsStringAsync();
+        }
+
+        public async Task<string> DeleteEmployeeAsync(int emp_id)
+        {
+            var json = JsonConvert.SerializeObject(new { emp_id });
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            var response = await client.PostAsync($"{baseUrl}/deleteEmployee", content);
+            return await response.Content.ReadAsStringAsync();
         }
     }
 }
